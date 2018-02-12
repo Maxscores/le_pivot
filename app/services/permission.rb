591297aee608646274e70238c1cc81d4a6fe053a
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-12 08:23:25 -0700

    permissions

--- app/services/permission.rb
+++ app/services/permission.rb
@@ -16,6 +16,7 @@ class Permission
     elsif user.registered_user?
       # return registered_user_permissions
       # Using the private method for registered_user_permissions oddly causes 3 tests to fail?
+      return true if controller == "store_orders" && action == "update"
       return true if controller == "main" && action == "index"
       return true if controller == "sessions" && action.in?(%w(new create destroy))
       return true if controller == "users" && action.in?(%w(new create edit update))
@@ -52,6 +53,8 @@ class Permission
       return true if controller == "items" && action.in?(%w(index show edit update new create))
       return true if controller == "auth" && action.in?(%w(twitter))
       return true if controller == "twitter" && action.in?(%w(edit update create destroy))
+      return true if controller == "cart" && action.in?(%w(index create edit destroy update))
+
     end
 
     def store_admin_permissions
@@ -75,6 +78,7 @@ class Permission
     def store_manager_permissions
       return true if controller == "store_orders"&& action.in?(%w(show create edit update destroy))
 
+      return true if controller == "store_orders" && action == "update"
       return true if controller == "admin/items" && action.in?(%w(index new create edit update destroy))
       return true if controller == "store/orders" && action.in?(%w(index new create edit update destroy))
       return true if controller == "main" && action == "index"
@@ -105,6 +109,7 @@ class Permission
     # end
 
     def base_permissions
+      return true if controller == "store_orders" && action == "update"
       return true if controller == "charges" && action.in?(%w(new create))
       return true if controller == "api/v1/search" && action.in?(%w(index))
       return true if controller == "developer" && action.in?(%w(show create))

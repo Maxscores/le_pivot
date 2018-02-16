ee825bfccb03782345d7d9d3e5a394495964b3e6
maxscores <maxstackhouse@gmail.com>
2018-02-16 08:10:24 -0700

    Fix Unauthorized Request Bug

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,8 +2,18 @@ class Chatroom < ApplicationRecord
   belongs_to :user
   has_many :chat_messages
 
+  after_save :send_greeting
+
   def unread_messages
     # Could lead to an awesome service
     "None"
+  end
+
+  def send_greeting
+    ChatMessage.create(user: User.find(1), chatroom: self, body: greeting)
+  end
+
+  def greeting
+    "Hello, Welcome to Found Sound. Let me know if you need any help"
   end
 end

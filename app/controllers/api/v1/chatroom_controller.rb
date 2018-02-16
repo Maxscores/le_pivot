f7943fadab20f8d4c340949db7f36fde003c3b97
maxscores <maxstackhouse@gmail.com>
2018-02-16 07:33:32 -0700

    have each api validate their own responses

--- app/controllers/api/v1/chatroom_controller.rb
+++ app/controllers/api/v1/chatroom_controller.rb
@@ -1,5 +1,15 @@
-class Api::V1::ChatroomController < ApplicationController
+class Api::V1::ChatroomController < Api::V1::BaseController
+  before_action :check_vaild_request
+
   def index
     render json: Chatroom.all
+  end
+
+  private
+
+  def check_vaild_request
+    if ApiKeyService.new(params['api_key']).invalid?
+      render json: invalid_request_response
+    end
   end
 end
--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -8,4 +8,17 @@ class Api::V1::SearchController < Api::V1::BaseController
       invalid_request
     end
   end
+
+  private
+    def check_vaild_request
+      if invalid_request
+        render json: invalid_request_response
+      end
+    end
+
+    def invalid_request
+      return true if params['type'].nil?
+      return true if params['q'].nil?
+      return true if ApiKeyService.new(params['api_key']).invalid?
+    end
 end
